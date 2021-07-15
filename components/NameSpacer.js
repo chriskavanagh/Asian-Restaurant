817ee0d4bc967a58584e213b1ccff9fc8651a123
Chris Kavanagh <ckava3@gmail.com>
2021-07-15 10:59:29 -0400

    css

--- components/NameSpacer.js
+++ components/NameSpacer.js
@@ -31,7 +31,7 @@ export default function NameSpacer() {
               font-size: 1rem;
             }
             .nameHead {
-              font-size: 1.7rem;
+              font-size: 1.6rem;
               color: gold;
             }
         `}
