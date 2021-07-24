616138bfbc232040d00a2bfa6930e2974df32a7b
Chris Kavanagh <ckava3@gmail.com>
2021-07-23 20:57:03 -0400

    NameSpacer change

--- components/NameSpacer.js
+++ components/NameSpacer.js
@@ -6,13 +6,8 @@ export default function NameSpacer() {
           <div className="col">
             <div className="d-flex flex-column align-items-center p-2">
               <h1 className="mb-4">Szechuan`s Restaurant</h1>
-<<<<<<< HEAD
-              <h5>4987 Bernard Dr Roanoke, VA</h5>
-              <h6>(540) 989-9487</h6>
-=======
               <h5 className="text-white">4987 Bernard Dr Roanoke, VA</h5>
               <h6 className="text-white">(540) 989-9487</h6>
->>>>>>> testing
             </div>
           </div>
         </div>
@@ -30,22 +25,14 @@ export default function NameSpacer() {
           }
           @media screen and (max-width: 780px) {
             h1 {
-<<<<<<< HEAD
-              font-size: 1.3rem;
-=======
               font-size: 1.4rem;
->>>>>>> testing
               letter-spacing: 1px !important;
             }
             h5 {
               font-size: 1.1rem;
             }
             h6 {
-<<<<<<< HEAD
-              font-size: 1.1rem;
-=======
               font-size: 1rem;
->>>>>>> testing
             }
           }
         `}
