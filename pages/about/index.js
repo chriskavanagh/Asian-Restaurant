c111599579557219b1e66891a22d6ef352729a19
Chris Kavanagh <ckava3@gmail.com>
2021-07-25 02:42:32 -0400

    about page

---
+++ pages/about/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function index() {
+  return (
+    <section className="container-fluid">
+      <div className="row">
+        <div className="col-lg-12">
+          <div className="d-flex justify-content-center mt-4">
+            <h1 className="top__h1">About Page!</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
